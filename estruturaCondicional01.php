da46b188744e6849603c030c60216e4999a8d727
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 13:32:57 -0300

    Update estruturaCondicional01.php

--- estruturaCondicional01.php
+++ estruturaCondicional01.php
@@ -1,15 +1,19 @@
 <?php
-
+    //define a variável "idade" como o numero inteiro "18" 
     $idade = 18;
-
+    //validação "se" para caso a "idade" seja maior ou igual a "18"
     if ($idade >= 18)
     {
+        //define a variavel "x" como string(texto) "É maior de idade" 
+        //lembrando que para definir uma variavel com valor de string, é inserido o valor entre aspas (") 
         $x = "É maior de idade";
     }
+    //validação "senao" caso idade não seja maior ou igual a 18
     else
     {
+        //define a variavel "x" com a string "É menor de idade"
         $x = "É menor de idade";
     }
-
+    //imprime na tela a variavel "x", que anteriormente foi definida na validação "se" ou na validação "senao"
     echo $x;
 ?>
