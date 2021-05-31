5dc8a739347cb1a9db22c9f998ca092ddbe24e6d
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 17:41:27 -0300

    Update estruturaPHP04.php

--- estruturaPHP04.php
+++ estruturaPHP04.php
@@ -1,6 +1,9 @@
 <?php
-	/* Aqui está o comentário por bloco
-	echo "Exemplo 01";
-	*/
+	//comentarios podem ser escritos de duas formas, com duas barras(//) ou por bloco como no exemplo abaixo
+	// O comentário por bloco inicia com /*	e é finalizado por */
+	/*isso significa que pode comentar em varias linhas
+	sem precisar dar // em cada linha
+	podendo escrever um texto completo com novas linhas e detalhes mais especificos
+	e finalizando o comentário no código inserindo no final */
 	echo "Imprimindo teste";
 ?>
