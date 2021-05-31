6576860fd1872f2cb277ab811226272407fd206d
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 13:01:27 -0300

    Update constantes.php

--- constantes.php
+++ constantes.php
@@ -4,9 +4,8 @@
     define( 'NOME', 'Jéssica Almeida' ); 
     define ('ALTURA', 1.70); 
     define('ATIVO', true);
-    
+    //escreve as constantes definidas acima na tela ao executar o código
     echo NOME . "\n";
     echo ALTURA . "\n";
-    echo ATIVO . "\n";
-    
+    echo ATIVO . "\n";    
 ?>
