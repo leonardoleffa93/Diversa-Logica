a3444cd9441e13862e6df3dd7504ed44fcd8b3da
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 17:35:53 -0300

    Update estruturaPHP02.php

--- estruturaPHP02.php
+++ estruturaPHP02.php
@@ -1,3 +1,5 @@
 <?php
+	//no código abaixo, é executado o comando "echo" para imprimir outro texto
+	//se atentando que para a impressão de texto o mesmo deve ser inserido entre aspas(")
 	echo "Aqui estou escrevendo uma programa em PHP ";
 ?>
