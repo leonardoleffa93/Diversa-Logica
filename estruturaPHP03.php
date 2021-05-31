50bbb8b01027e3983bd8b64c482e23c286452662
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 17:37:43 -0300

    Update estruturaPHP03.php

--- estruturaPHP03.php
+++ estruturaPHP03.php
@@ -1,4 +1,5 @@
 <?php
-	//Aqui está o comentário
-	echo "Exemplo de comentário com uma unica linha.";
+	//este seria um exemplo de comentário, que seria este texto mesmo com as duas barras (//) no inicio da linha
+	//ele não é lido como código, e serve para fazer menções dentro do código que são interessantes de guardar para saber do que se trata o código futuramente
+	echo "Exemplo de comentário";
 ?>
