3dc9df25b40cf9e939b30b821ea3457501e393b9
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 19:50:24 -0300

    Update expressoesAritmeticas01.php

--- expressoesAritmeticas01.php
+++ expressoesAritmeticas01.php
@@ -1,5 +1,8 @@
 <?php
-
+    /*
+    comando echo para imprimir na tela uma informação
+    dentro de cada echo uma expressão aritmetica
+    */
     echo (5 % 3)."\n";           // imprime 2
     echo (5 * -3)."\n";          // imprime -15
     echo (-5 - 3)."\n";          // imprime -8
