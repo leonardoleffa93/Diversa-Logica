b43868d942b6c253537eba24348fd25d91dada83
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 19:47:38 -0300

    Update estruturaRepetitiva03.php

--- estruturaRepetitiva03.php
+++ estruturaRepetitiva03.php
@@ -1,8 +1,9 @@
 <?php
 
-    /* O bloco de comando irá repetir enquanto o $i < 10
-      Irá iniciar com 0
-     Repetirá 10 vezes (De 0 a 9)
+    /* 
+    O bloco de comando irá repetir enquanto o $i < 10
+    Irá iniciar com 0
+    Repetirá 10 vezes (De 0 a 9)
     */
 
     for ($i=0; $i < 10; $i++) {
