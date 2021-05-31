d1677e9e6fb1581a42a9bbf3d8eb7e44125c50ec
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 12:16:32 -0300

    Update conceitosIniciais01.php

--- conceitosIniciais01.php
+++ conceitosIniciais01.php
@@ -1,10 +1,11 @@
 <?php
-
+    //echo serve para imprimir na tela algo escrito sem formatação
+    //as aspas ("") são pra definir como texto (string) as palavras escritas, e não como código.
     echo "Olhando para a direita. \n";
     echo "Olhando para a esquerda.\n";
-    
+    //variavel temCarro definida como a string "sim";
     $temCarro = "sim";
-
+    //verificação "se" para ver se "temCarro" é definido como "sim"
     if($temCarro == "sim")
     {
         echo "Não atravessei a rua";
