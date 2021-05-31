bb53105426386532c6588030d7c1073a57860e87
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 19:49:16 -0300

    Update estruturaRepetitiva03.php

--- estruturaRepetitiva03.php
+++ estruturaRepetitiva03.php
@@ -1,6 +1,8 @@
 <?php
 
-    /* 
+    /*
+    a definição de uma variavel para repetição pode ser definida nas configurações da mesma
+    no código é definido a variável "i" como igual a "0"
     O bloco de comando irá repetir enquanto o $i < 10
     Irá iniciar com 0
     Repetirá 10 vezes (De 0 a 9)
