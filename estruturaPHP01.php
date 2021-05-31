51c4d3d3e5421322f196ffdd96b84379ae3f82b4
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 17:34:51 -0300

    Update estruturaPHP01.php

--- estruturaPHP01.php
+++ estruturaPHP01.php
@@ -1,3 +1,5 @@
 <?php
+	//a tag <?php acima é para identificar o código dentro do arquivo como um script php
+	//o comando "echo", no php, imprime na tela algum código logo após a execução dele. no exemplo abaixo, o texto "Olá, mundo! "
 	echo "Olá, mundo! ";
 ?>
