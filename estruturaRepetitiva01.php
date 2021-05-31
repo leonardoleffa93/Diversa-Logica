921fb38c6601ee031c72a7c9291c028080bebb45
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-05-31 18:36:36 -0300

    Update estruturaRepetitiva01.php

--- estruturaRepetitiva01.php
+++ estruturaRepetitiva01.php
@@ -1,9 +1,8 @@
 <?php
 
-    // O bloco de comando irá repetir enquanto o $i <= 10
-    
+    //declaração de variavel "i" como numero "1"        
     $i = 1;
-
+    // O bloco de comando irá repetir enquanto o $i <= 10
     while ($i <= 10) {
         echo $i . "\n";
         $i++;
