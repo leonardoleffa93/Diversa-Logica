a78b6fe18d5f80ce8635969718215d579fecf8ea
leonardoleffa93 <85121622+leonardoleffa93@users.noreply.github.com>
2021-09-21 12:58:17 -0300

    Create exemploarray.php

---
+++ exemploarray.php
@@ -0,0 +1,7 @@
+<?php
+    $carros = array("Fiat","Ford","volkswagen");
+    echo "Eu tenho um carro da marca " . $carros[0] . ".\n";
+    $idade = array('18', '22', '27');
+    $nome = array("Pedro", "Leonardo", "Ana");
+    echo "Eu me chamo " . $nome[1] . " e tenho " . $idade[2] . " anos de idade"
+?>
